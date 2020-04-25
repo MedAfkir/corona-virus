6d873fbee5eb221c0bb5ff2cd008a5a999740e3f
MedAfkir <afkir_mohamed_01@hotmail.fr>
2020-04-25 17:07:07 +0000

    Made Chart responsive

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,106 +1,140 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { PieChart, Pie, Sector } from "recharts";
 import { colors } from "./../../utils";
 import { Context } from "./../../Context";
 
-const renderActiveShape = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  startAngle,
-  endAngle,
-  fill,
-  payload,
-  percent,
-  value,
-}) => {
-  const RADIAN = Math.PI / 180;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? "start" : "end";
-
-  return (
-    <g>
-      <text
-        x={cx}
-        y={cy}
-        dy={8}
-        textAnchor="middle"
-        fill="#252525"
-        style={{ fontSize: 17, fontWeight: 700 }}
-      >
-        {payload.name}
-      </text>
-      <Sector
-        cx={cx}
-        cy={cy}
-        innerRadius={innerRadius}
-        outerRadius={outerRadius}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        fill={fill}
-      />
-      <Sector
-        cx={cx}
-        cy={cy}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        innerRadius={outerRadius + 6}
-        outerRadius={outerRadius + 12}
-        fill={fill}
-      />
-      <path
-        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-        stroke={fill}
-        fill="none"
-      />
-      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        textAnchor={textAnchor}
-        fill="#333"
-        style={{ fontWeight: 700 }}
-      >
-        {value}
-      </text>
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        dy={18}
-        textAnchor={textAnchor}
-        fill="#817c9b"
-      >{`${(percent * 100).toFixed(2)}%`}</text>
-    </g>
-  );
-};
-
-const Chart = (props) => {
+const Chart = ({ ...props }) => {
+  // Max width for small devices
+  const maxWidth = 560;
   const [activeIndex, setActiveIndex] = useState(0);
+  const [smallDevice, setSmallDevice] = useState(window.innerWidth <= maxWidth);
+
   const {
     data: { confirmed, recovered, deaths },
   } = useContext(Context);
 
-  // Calcul active cases
-  const active = confirmed.value - recovered.value - deaths.value;
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setSmallDevice(window.innerWidth <= maxWidth);
+    });
+  }, []);
 
   // Pie Chart Data
+  const active = confirmed.value - recovered.value - deaths.value;
   const pieData = [
     { name: "Active", value: active, fill: colors.main },
     { name: "Recovered", value: recovered.value, fill: colors.green },
     { name: "Deaths", value: deaths.value, fill: colors.red },
   ];
 
-  const onPieEnter = (data, index) => {
-    setActiveIndex(index);
+  const onPieEnter = (data, index) => setActiveIndex(index);
+
+  const renderActiveShape = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  }) => {
+    const RADIAN = Math.PI / 180;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+
+    return (
+      <g>
+        <text
+          x={cx}
+          y={smallDevice ? cy - 23 : cy}
+          dy={8}
+          textAnchor="middle"
+          fill="#252525"
+          style={{ fontWeight: 700 }}
+        >
+          {payload.name}
+        </text>
+        {smallDevice && (
+          <>
+            <text
+              x={cx}
+              y={cy}
+              dy={8}
+              textAnchor="middle"
+              fill={fill}
+              style={{ fontWeight: 700, letterSpacing: 0.5 }}
+            >
+              {value}
+            </text>
+            <text
+              x={cx}
+              y={cy + 20}
+              dy={8}
+              textAnchor="middle"
+              fill="#817c9b"
+              style={{ fontSize: 14 }}
+            >
+              {`${(percent * 100).toFixed(2)}%`}
+            </text>
+          </>
+        )}
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        {!smallDevice && (
+          <>
+            <Sector
+              cx={cx}
+              cy={cy}
+              startAngle={startAngle}
+              endAngle={endAngle}
+              innerRadius={outerRadius + 6}
+              outerRadius={outerRadius + 12}
+              fill={fill}
+            />
+            <path
+              d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+              stroke={fill}
+              fill="none"
+            />
+            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+            <text
+              x={ex + (cos >= 0 ? 1 : -1) * 12}
+              y={ey}
+              textAnchor={textAnchor}
+              fill="#333"
+              style={{ fontWeight: 700 }}
+            >
+              {value}
+            </text>
+            <text
+              x={ex + (cos >= 0 ? 1 : -1) * 12}
+              y={ey}
+              dy={18}
+              textAnchor={textAnchor}
+              fill="#817c9b"
+            >{`${(percent * 100).toFixed(2)}%`}</text>
+          </>
+        )}
+      </g>
+    );
   };
 
   return (
