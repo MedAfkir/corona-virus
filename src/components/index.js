cd681e0be4273ade839e742d099c043cb2391748
MedAfkir <afkir_mohamed_01@hotmail.fr>
2020-04-25 15:47:27 +0000

    Export all components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Error from "./Error";
+import SpinnerLoader from "./Loader";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import Content from "./Content";
+
+export { Error, SpinnerLoader, Header, SideBar, Content };
