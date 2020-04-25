eb5a91314d62fb779ed0789111de73414e7e32f3
MedAfkir <afkir_mohamed_01@hotmail.fr>
2020-04-25 15:51:03 +0000

    Export Notification components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import SpinnerLoader from "./Loader";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import Content from "./Content";
+import Notification from "./Notification";
 
-export { Error, SpinnerLoader, Header, SideBar, Content };
+export { Error, SpinnerLoader, Header, SideBar, Content, Notification };
